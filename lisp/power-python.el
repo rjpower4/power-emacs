1b774a311eed83f36565984480317c0f6ad5ae2b
Rolfe Power <rolfepower4@gmail.com>
2021-05-06 17:19:58 -0400

    Add pyright

--- lisp/power-python.el
+++ lisp/power-python.el
@@ -22,7 +22,9 @@
     (add-to-list 'company-backends 'company-anaconda)))
 
 
-
+(use-package lsp-pyright
+  :defer t
+  :hook (python-mode . (lambda () (require 'lsp-pyright))))
 
 (provide 'power-python)
 ;;; power-python.el ends here
