6b2a8167546f0026b827798706c14ecbfd81e18a
Rolfe Power <rolfepower4@gmail.com>
2021-05-19 15:29:20 -0400

    Modus theme setting modifications

--- init.el
+++ init.el
@@ -762,7 +762,23 @@ ring."
 (use-package modus-themes
   :straight t
   :custom
+  (modus-themes-bold-constructs t)
+  (modus-themes-completions 'nil)
   (modus-themes-no-mixed-fonts t)
+  (modus-themes-diffs 'desaturated)
+  (modus-themes-fringes 'intense)
+  (modus-themes-lang-checkers 'colored-background)
+  (modus-themes-links nil)
+  (modus-themes-mail-citations nil)
+  (modus-themes-mode-line nil)
+  (modus-themes-org-blocks nil)
+  (modus-themes-paren-match 'intense-bold)
+  (modus-themes-prompts 'intense-gray)
+  (modus-themes-region 'bg-only-no-extend)
+  (modus-themes-scale-headings nil)
+  (modus-themes-syntax 'yellow-comments-green-strings)
+  (modus-themes variable-pitch-headings nil)
+  (modus-themes-variable-pitch-ui nil)
   (modus-themes-slanted-constructs t))
 
 ;; Theme Selection
