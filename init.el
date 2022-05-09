29ebea2620cc7572b2f5352f5a3e0ef40043f502
Rolfe Power <rolfepower4@gmail.com>
2022-05-09 12:09:30 -0400

    Update the path to my org folder, now a subdirectory of notes

--- init.el
+++ init.el
@@ -56,7 +56,7 @@
 (defconst power-etc-dir (concat power-local-dir "etc/"))
 (defconst power-org-dir
   (if IS-WINDOWS
-      "C:\\Users\\rolfe\\Dropbox\\Org\\"
+      "C:\\Users\\rolfe\\Dropbox\\notes\\org\\"
     (concat (file-name-as-directory (getenv "HOME")) "Dropbox/Org/")))
 
 ;; -- Location
